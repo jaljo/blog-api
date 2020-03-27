7e609c293e97e71b179ceff41433d365a6a23da5
Joris Langlois <joris.langlois@knplabs.com>
2020-03-27 16:47:19 +0100

    Add a publish endpoint

--- src/Application/Controller/Endpoint.php
+++ src/Application/Controller/Endpoint.php
@@ -3,10 +3,13 @@
 namespace App\Application\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 interface Endpoint
 {
     public function list(): JsonResponse;
 
     public function read(string $slug): JsonResponse;
+
+    public function create(Request $request): JsonResponse;
 }
